a0698855687c5d2f5d53caa8bb3ec600c21283ca
Mike Devita <mike@relative.media>
2016-12-18 06:45:32 -0700

    resolve merge conflicts

--- tools/setup/setupMessage.js
+++ tools/setup/setupMessage.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 // This script displays an intro message for the setup script
 /* eslint-disable no-console */
 console.log('===========================')
 console.log('=  React Slingshot Setup  =')
 console.log('===========================\n')
 console.log('Installing dependencies. Please wait...')
-=======
-// This script displays an intro message for the setup script
-/* eslint-disable no-console */
-console.log('===========================');
-console.log('=  React Slingshot Setup  =');
-console.log('===========================\n');
-console.log('Installing dependencies. Please wait...');
->>>>>>> 46395453ddd1aec93d2bb13750a92be3e7b76cf8
--- tools/setup/setupPrompts.js
+++ tools/setup/setupPrompts.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Define prompts for use with npm 'prompt' module in setup script
 module.exports = [
   {
@@ -25,31 +24,3 @@ module.exports = [
     description: 'Project description'
   }
 ]
-=======
-// Define prompts for use with npm 'prompt' module in setup script
-module.exports = [
-  {
-    name: 'projectName',
-    description: 'Project name (default: new-project)',
-    pattern: /^[^._][a-z0-9\.\-_~]+$/,
-    message: 'Limited to: lowercase letters, numbers, period, hyphen, ' +
-    'underscore, and tilde; cannot begin with period or underscore.'
-  },
-  {
-    name: 'version',
-    description: 'Version (default: 0.1.0)'
-  },
-  {
-    name: 'author',
-    description: 'Author'
-  },
-  {
-    name: 'license',
-    description: 'License (default: MIT)'
-  },
-  {
-    name: 'description',
-    description: 'Project description'
-  }
-];
->>>>>>> 46395453ddd1aec93d2bb13750a92be3e7b76cf8
